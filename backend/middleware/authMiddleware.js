2cc72d6ac505806023b3e89ed3870345ecdd19cd
peet5228 <ppeet5228@gmail.com>
2025-11-29 19:29:55 -0800

    update3

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -7,7 +7,7 @@ exports.verifyToken = (req,res,next) => {
     const token = authHeader.split(" ")[1]
     try{
         req.user = jwt.verify(token,JWT_SECRET)
-        nexrt()
+        next()
     }catch(err){
         console.error('Invalid Token',err)
         res.status(403).json({message:'Invalid Token'})
--- backend/routes/Staff/indicate.js
+++ backend/routes/Staff/indicate.js
@@ -39,17 +39,30 @@ router.post('/',verifyToken,requireRole('ฝ่ายบุคลากร'),asy
     }
 })
 
-// API สำหรับ Get ข้อมูล
+// API สำหรับ Update ข้อมูล
 router.put('/:id_indicate',verifyToken,requireRole('ฝ่ายบุคลากร'),async (req,res) => {
     try{
         const {id_indicate} = req.params
         const {id_topic,name_indicate,detail_indicate,point_indicate,check_indicate} = req.body
-        const [rows] = await db.query(`update tb_indicate set id_topic=?,name_indicate=?,detail_indicate=?,point_indicate=?,check_indicate=?`)
-        res.json(rows)
+        const [rows] = await db.query(`update tb_indicate set id_topic=?,name_indicate=?,detail_indicate=?,point_indicate=?,check_indicate=? where id_indicate='${id_indicate}'`,[id_topic,name_indicate,detail_indicate,point_indicate,check_indicate])
+        res.json({rows,message:'Update Success'})
     }catch(err){
         console.error("Error Get",err)
         res.status(500).json({message:'Error Get'})
     }
 })
+
+// API สำหรับ Delete ข้อมูล
+router.delete('/:id_indicate',verifyToken,requireRole('ฝ่ายบุคลากร'),async (req,res) => {
+    try{
+        const {id_indicate} = req.params
+        const [rows] = await db.query(`delete from tb_indicate where id_indicate='${id_indicate}'`)
+        res.json({rows,message:'Delete Success'})
+    }catch(err){
+        console.error("Error Delete",err)
+        res.status(500).json({message:'Error Delete'})
+    }
+})
+
 
 module.exports = router
